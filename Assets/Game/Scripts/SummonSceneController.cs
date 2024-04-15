ae969fc1c709313d2e2b33f7732adf7642fd69f7
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2024-04-16 00:45:20 +0300

    WIP

--- Assets/Game/Scripts/SummonSceneController.cs
+++ Assets/Game/Scripts/SummonSceneController.cs
@@ -63,7 +63,7 @@ public class SummonSceneController : MonoBehaviour
             }
         }
 
-        // selectedMonster = _availableMonsters[3];
+        // selectedMonster = _availableMonsters[4];
 
         _ingredientsPanelController.DepleteSelectedIngredients();
 
