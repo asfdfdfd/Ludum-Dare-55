dfc72300c62411d02b0f79582ce9e1694748f786
Andrey Panchenko <asfdfdfd@asfdfdfd.com>
2024-04-16 01:45:27 +0300

    WIP.

--- Assets/Game/Scripts/BookSceneController.cs
+++ Assets/Game/Scripts/BookSceneController.cs
@@ -7,8 +7,49 @@ public class BookSceneController : MonoBehaviour
     [SerializeField]
     private GameObject gameObjectBook;
 
+    [SerializeField]
+    private GameObject gameObjectPanel1;
+
+    [SerializeField]
+    private GameObject gameObjectPanel2;    
+
+    [SerializeField]
+    private GameObject gameObjectPanel3;    
+
+    private int panelIndex = 0;
+
+    private readonly List<GameObject> panels = new List<GameObject>();
+
+    private void Awake() 
+    {
+        panels.Add(gameObjectPanel1);
+        panels.Add(gameObjectPanel2);
+        panels.Add(gameObjectPanel3);
+    }
+
     public void OnBookCloseButtonClick()
     {
         gameObjectBook.SetActive(false);
+    }
+
+    public void ShowPanel1() 
+    {
+        gameObjectPanel1.SetActive(true);
+        gameObjectPanel2.SetActive(false);
+        gameObjectPanel3.SetActive(false);
+    }
+
+    public void ShowPanel2() 
+    {
+        gameObjectPanel1.SetActive(false);
+        gameObjectPanel2.SetActive(true);
+        gameObjectPanel3.SetActive(false);        
+    }    
+
+    public void ShowPanel3() 
+    {
+        gameObjectPanel1.SetActive(false);
+        gameObjectPanel2.SetActive(false);
+        gameObjectPanel3.SetActive(true);        
     }
 }
